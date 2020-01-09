df33f2639746e88ad739a6360ec6df398b95cb7c
danil <danil@DESKTOP-P2C854C>
2020-01-09 17:47:14 +0000

    Kata 5-2

---
+++ src/Kata52/Kata52.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Kata52;
+import Model.*;
+import java.io.IOException;
+import java.util.List;
+import View.*;
+
+/**
+ *
+ * @author danil
+ */
+
+public class Kata52 {
+
+    private static List<String> listMails;
+    private static Histogram histograma;
+    public static void main(String[] args) throws IOException {
+        input();
+        process();
+        output();
+    }
+   
+     public static void input() throws IOException{
+        MailListReaderBD bd = new MailListReaderBD();
+        listMails = bd.read("jdbc:sqlite:Kata5.db", "EMAIL");
+    }
+    
+    public static void process(){
+        MailHistogramBuilder builded = new MailHistogramBuilder();
+        histograma = builded.build(listMails);
+
+    }
+    
+    public static void output(){
+        new HistogramDisplay(histograma).execute();
+    }
+}
